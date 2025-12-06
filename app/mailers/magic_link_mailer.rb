b7626158014ef1bec1b54c4f728d3a389ded8f28
davdiaz <ddiaz_2k2@yahoo.es>
2025-12-06 12:35:15 +0100

    Update magic_link_mailer.rb

--- app/mailers/magic_link_mailer.rb
+++ app/mailers/magic_link_mailer.rb
@@ -2,7 +2,9 @@ class MagicLinkMailer < ApplicationMailer
   def sign_in_instructions(magic_link)
     @magic_link = magic_link
     @identity = @magic_link.identity
-
+    # Log directo para ver que entramos aquí
+    Rails.logger.info "[MAGIC LINK MAILER] Preparing magic link email for #{@magic_link.email}"
+    
     mail to: @identity.email_address, subject: "Your Fizzy code is #{ @magic_link.code }"
   end
 end
